c965dd83e16cab35de4202e91e648596103bf99b
tramxme <tramlaisf@gmail.com>
2015-10-25 00:54:05 -0700

    add some methods to get opcode and immediate

--- Validate.java
+++ Validate.java
@@ -3,57 +3,63 @@ import java.io.*;
 
 public class Validate {
 
+   private Instruction.OpCode o;
    private String opCode;
    private String registers = "";
    private String[] reg = {};
+   private Integer immd;
 
    public Validate(String ins) {
       opCode = ins.trim();
       if(ins.contains("$")){
-          opCode = ins.substring(0, ins.indexOf("$")).trim();
-          registers = ins.substring(ins.indexOf("$"), ins.length()).trim();
-          reg = registers.split(",");
+         opCode = ins.substring(0, ins.indexOf("$")).trim();
+         registers = ins.substring(ins.indexOf("$"), ins.length()).trim();
+         reg = registers.split(",");
       }
    }
 
    public boolean CheckSyntax() {
-       if(Instruction.getCode(opCode) == null) {
+      if(Instruction.getCode(opCode) == null) {
          return false;
       }
       else {
-         Instruction.OpCode o = Instruction.getCode(opCode);
+         o = Instruction.getCode(opCode);
 
          //Check if it has enough arguments
          if(reg.length != o.params) {
             return false;
          }
+
          //If destination and source are not registers
-         else if(o.params >= 2 && (!reg[0].contains("$") || !reg[1].contains("$"))) {
+         if(o.params >= 2 && (!reg[0].contains("$") || !reg[1].contains("$"))) {
             return false;
          }
+
          //If add, addu, and, or, sltu, sub and the 3rd value is not a register
-         else if ((o == Instruction.OpCode.ADD ||
-                     o == Instruction.OpCode.ADDU ||
-                     o == Instruction.OpCode.AND ||
-                     o == Instruction.OpCode.OR ||
-                     o == Instruction.OpCode.SLTU ||
-                     o == Instruction.OpCode.SUB ) && !reg[2].contains("$"))
-               {
-                  return false;
-               }
-               //If sll, sra, srl, addi, addiu, andi, ori, sltiu, lui
-               else if ((o == Instruction.OpCode.SLL ||
-                         o == Instruction.OpCode.SRA ||
-                         o == Instruction.OpCode.SRL ||
-                         o == Instruction.OpCode.ADDI ||
-                         o == Instruction.OpCode.ADDIU ||
-                         o == Instruction.OpCode.ORI ||
-                         o == Instruction.OpCode.SLTIU ||
-                         o == Instruction.OpCode.LUI)
-                        && reg[o.params - 1].matches("[^0-9]")){
-                  return false;
-               }
+         if ((o == Instruction.OpCode.ADD || o == Instruction.OpCode.ADDU || o == Instruction.OpCode.AND || o == Instruction.OpCode.OR ||
+                  o == Instruction.OpCode.SLTU || o == Instruction.OpCode.SUB) && !reg[2].contains("$"))
+         {
+            return false;
+         }
+
+         //If sll, sra, srl, addi, addiu, andi, ori, sltiu, lui
+         if ((o == Instruction.OpCode.SLL || o == Instruction.OpCode.SRA || o == Instruction.OpCode.SRL || o == Instruction.OpCode.ADDI ||
+                  o == Instruction.OpCode.ADDIU || o == Instruction.OpCode.ORI || o == Instruction.OpCode.SLTIU || o == Instruction.OpCode.LUI)){
+            if(!reg[o.params - 1].matches("[-+]?\\d*")){
+               return false;
+            }else{
+               immd = Integer.parseInt(reg[o.params-1]);
+            }
+                  }
       }
       return true;
+   }
+
+   public Instruction.OpCode getOpCode() {
+      return o;
+   }
+
+   public Integer getImmediate() {
+      return immd;
    }
 }
